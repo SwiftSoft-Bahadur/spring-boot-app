ad417f8eeeb20b28f92433a46cb63e512d67b3d5
Bahadur Khan <70307617+SwiftSoft-Bahadur@users.noreply.github.com>
2023-12-17 10:46:44 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -2,6 +2,8 @@ package in.ashokit.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeRestController {
 
-	private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
+    private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
 
-	public WelcomeRestController() {
-		logger.info("***** WelcomeRestController::Constructor *****");
-	}
+    public WelcomeRestController() {
+        logger.info("***** WelcomeRestController::Constructor *****");
+    }
 
-	@GetMapping
-	public String welcomeMsg() {
-		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
-		logger.info("***** welcomeMsg() execution end *****");
-		return msg;
-	}
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcomeMsg() {
+        logger.info("***** welcomeMsg() execution start *****");
+        String msg = "Welcome to the Beautiful Spring Boot REST API!";
+        logger.info("***** welcomeMsg() execution end *****");
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    @GetMapping("/welcome/{name}")
+    public ResponseEntity<String> welcomeMsgWithName(@PathVariable String name) {
+        logger.info("***** welcomeMsgWithName() execution start *****");
+        String msg = String.format("Hello, %s! Welcome to the Beautiful Spring Boot REST API!", name);
+        logger.info("***** welcomeMsgWithName() execution end *****");
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
 }
